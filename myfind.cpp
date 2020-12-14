a84d7b442d1aa168a0c084c256738723da80a691
Guest <guest-smctq0@OSLAB.localdomain>
2020-12-14 10:50:35 +0800

    finish3

--- myfind.cpp
+++ myfind.cpp
@@ -11,12 +11,62 @@
 #include <vector>
 using namespace std;
 
+vector<struct dirent*> results;
+vector<struct dirent*>::iterator it;
+
+bool isdir(struct stat buf){
+	if(S_ISDIR(buf.st_mode)){
+		return 1;	
+	}
+	return 0;
+}
+
+void find(char *path, char *name, char *mtime){
+	DIR *target_path_dir = opendir(path);
+	struct dirent *target_path_file;
+
+	while(target_path_file = readdir(target_path_dir)){
+		if(strcmp(target_path_file->d_name, ".") == 0 ||
+			strcmp(target_path_file->d_name, "..") == 0)
+			continue;
+
+		char *search_result_path = new char[strlen(path) + strlen(target_path_file->d_name) + 1];
+		strcpy(search_result_path, path);
+		strcat(search_result_path, target_path_file->d_name);
+		struct stat statbuf;
+		int status = stat(search_result_path, &statbuf);
+
+		if (isdir(statbuf) == 0){
+			if(name != nullptr){
+				regex re_name(name);
+				if(regex_match(target_path_file->d_name, re_name)){
+					continue;
+				}	
+			}
+	
+			if(mtime != nullptr){
+				time_t nowtime = time(0);
+				int m = atoi(mtime);
+				if(status != -1){
+					if (nowtime - statbuf.st_mtime < m * 86400){
+						continue;
+					}
+				}					
+			}
+			results.push_back(target_path_file);
+		}
+		else if(isdir(statbuf) == 1){
+			find(search_result_path, name, mtime);
+		}
+		
+	}
+}
+
 int main(int argc, char *argv[]){
 	char *path;
 	char *name = nullptr;
 	char *mtime = nullptr;
-	vector<struct dirent*> results;
-	vector<struct dirent*>::iterator it;
+	
 	if(argc <= 1){
 		cout<<"Invalid Path Input!"<<endl;
 		return 0;
@@ -36,40 +86,9 @@ int main(int argc, char *argv[]){
 		}
 	}
 	//cout<<path<<" "<<name<<" "<<mtime<<endl;
-
-	DIR *target_path_dir = opendir(path);
-	struct dirent *target_path_file;
-
-	while(target_path_file = readdir(target_path_dir)){
-		if(strcmp(target_path_file->d_name, ".") == 0 ||
-			strcmp(target_path_file->d_name, "..") == 0)
-			continue;
-		results.push_back(target_path_file);
-
-	}
-	for(it = results.begin(); it != results.end(); it++){
-		if(name != nullptr){
-			regex re_name(name);
-			if(!regex_match((*it)->d_name, re_name)){
-				results.erase(it--);
-				continue;
-			}	
-		}
-		if(mtime != nullptr){
-			time_t nowtime = time(0);
-			int m = atoi(mtime);
-			char *search_result_path = new char[strlen(path) + strlen((*it)->d_name) + 1];
-			strcpy(search_result_path, path);
-			strcat(search_result_path, (*it)->d_name);
-			struct stat statbuf;
-			int status = stat(search_result_path, &statbuf);
-			if(status != -1){
-				if (nowtime - statbuf.st_mtime >= m * 86400){
-					results.erase(it--);
-				}
-			}					
-		}
-	}
+	
+	find(path, name, mtime);
+	
 	if(results.size() == 0){
 		cout<<"No File Found!"<<endl;
 	}
